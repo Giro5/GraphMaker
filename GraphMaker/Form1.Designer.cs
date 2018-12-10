658bc5e8d146dd50f089081e276c67970bbc879c
Nikita <39918253+Giro5@users.noreply.github.com>
2018-12-11 00:28:35 +0500

    Add files via upload

---
+++ GraphMaker/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace GraphMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.GraphicChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.YEqLbl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BuildBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphicChart)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GraphicChart
+            // 
+            this.GraphicChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GraphicChart.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea1.AxisX.LineWidth = 2;
+            chartArea1.AxisY.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.LineWidth = 2;
+            chartArea1.Name = "ChartArea1";
+            this.GraphicChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.GraphicChart.Legends.Add(legend1);
+            this.GraphicChart.Location = new System.Drawing.Point(0, 86);
+            this.GraphicChart.Margin = new System.Windows.Forms.Padding(5);
+            this.GraphicChart.Name = "GraphicChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Line";
+            this.GraphicChart.Series.Add(series1);
+            this.GraphicChart.Size = new System.Drawing.Size(1017, 631);
+            this.GraphicChart.TabIndex = 0;
+            this.GraphicChart.Text = "chart1";
+            // 
+            // YEqLbl
+            // 
+            this.YEqLbl.AutoSize = true;
+            this.YEqLbl.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.YEqLbl.Location = new System.Drawing.Point(12, 45);
+            this.YEqLbl.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.YEqLbl.Name = "YEqLbl";
+            this.YEqLbl.Size = new System.Drawing.Size(40, 22);
+            this.YEqLbl.TabIndex = 1;
+            this.YEqLbl.Text = "Y =";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(62, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(799, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // BuildBtn
+            // 
+            this.BuildBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BuildBtn.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BuildBtn.Location = new System.Drawing.Point(871, 37);
+            this.BuildBtn.Margin = new System.Windows.Forms.Padding(5);
+            this.BuildBtn.Name = "BuildBtn";
+            this.BuildBtn.Size = new System.Drawing.Size(125, 39);
+            this.BuildBtn.TabIndex = 3;
+            this.BuildBtn.Text = "Build";
+            this.BuildBtn.UseVisualStyleBackColor = true;
+            this.BuildBtn.Click += new System.EventHandler(this.BuildBtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1017, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 717);
+            this.Controls.Add(this.BuildBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.YEqLbl);
+            this.Controls.Add(this.GraphicChart);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "GraphMaker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GraphicChart)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart GraphicChart;
+        private System.Windows.Forms.Label YEqLbl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BuildBtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+    }
+}
+
