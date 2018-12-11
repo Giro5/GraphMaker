a5b2246da0dabd132d9201feb181791666ad1193
Nikita <39918253+Giro5@users.noreply.github.com>
2018-12-11 22:48:14 +0500

    Add files via upload

--- GraphMaker/Form1.Designer.cs
+++ GraphMaker/Form1.Designer.cs
@@ -37,6 +37,17 @@
             this.BuildBtn = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AxisXLbl = new System.Windows.Forms.Label();
+            this.XMin = new System.Windows.Forms.TextBox();
+            this.Semicolon1 = new System.Windows.Forms.Label();
+            this.XMax = new System.Windows.Forms.TextBox();
+            this.RightBracket1 = new System.Windows.Forms.Label();
+            this.RightBracket2 = new System.Windows.Forms.Label();
+            this.YMax = new System.Windows.Forms.TextBox();
+            this.Semicolon2 = new System.Windows.Forms.Label();
+            this.YMin = new System.Windows.Forms.TextBox();
+            this.AxisYLbl = new System.Windows.Forms.Label();
+            this.AutoY = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.GraphicChart)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -59,12 +70,13 @@
             this.GraphicChart.Location = new System.Drawing.Point(0, 86);
             this.GraphicChart.Margin = new System.Windows.Forms.Padding(5);
             this.GraphicChart.Name = "GraphicChart";
+            this.GraphicChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series1.Legend = "Legend1";
             series1.Name = "Line";
             this.GraphicChart.Series.Add(series1);
-            this.GraphicChart.Size = new System.Drawing.Size(1017, 631);
+            this.GraphicChart.Size = new System.Drawing.Size(1017, 632);
             this.GraphicChart.TabIndex = 0;
             this.GraphicChart.Text = "chart1";
             // 
@@ -92,8 +104,7 @@
             // 
             // BuildBtn
             // 
-            this.BuildBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BuildBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.BuildBtn.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.BuildBtn.Location = new System.Drawing.Point(871, 37);
             this.BuildBtn.Margin = new System.Windows.Forms.Padding(5);
@@ -120,11 +131,132 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
+            // AxisXLbl
+            // 
+            this.AxisXLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.AxisXLbl.AutoSize = true;
+            this.AxisXLbl.Location = new System.Drawing.Point(12, 726);
+            this.AxisXLbl.Name = "AxisXLbl";
+            this.AxisXLbl.Size = new System.Drawing.Size(100, 22);
+            this.AxisXLbl.TabIndex = 5;
+            this.AxisXLbl.Text = "Axis X: [";
+            // 
+            // XMin
+            // 
+            this.XMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.XMin.Location = new System.Drawing.Point(109, 723);
+            this.XMin.Name = "XMin";
+            this.XMin.Size = new System.Drawing.Size(45, 30);
+            this.XMin.TabIndex = 6;
+            this.XMin.Text = "-10";
+            // 
+            // Semicolon1
+            // 
+            this.Semicolon1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Semicolon1.AutoSize = true;
+            this.Semicolon1.Location = new System.Drawing.Point(154, 726);
+            this.Semicolon1.Name = "Semicolon1";
+            this.Semicolon1.Size = new System.Drawing.Size(20, 22);
+            this.Semicolon1.TabIndex = 7;
+            this.Semicolon1.Text = ";";
+            // 
+            // XMax
+            // 
+            this.XMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.XMax.Location = new System.Drawing.Point(170, 723);
+            this.XMax.Name = "XMax";
+            this.XMax.Size = new System.Drawing.Size(45, 30);
+            this.XMax.TabIndex = 8;
+            this.XMax.Text = "10";
+            // 
+            // RightBracket1
+            // 
+            this.RightBracket1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.RightBracket1.AutoSize = true;
+            this.RightBracket1.Location = new System.Drawing.Point(215, 726);
+            this.RightBracket1.Name = "RightBracket1";
+            this.RightBracket1.Size = new System.Drawing.Size(20, 22);
+            this.RightBracket1.TabIndex = 9;
+            this.RightBracket1.Text = "]";
+            // 
+            // RightBracket2
+            // 
+            this.RightBracket2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.RightBracket2.AutoSize = true;
+            this.RightBracket2.Location = new System.Drawing.Point(497, 726);
+            this.RightBracket2.Name = "RightBracket2";
+            this.RightBracket2.Size = new System.Drawing.Size(20, 22);
+            this.RightBracket2.TabIndex = 14;
+            this.RightBracket2.Text = "]";
+            // 
+            // YMax
+            // 
+            this.YMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.YMax.Enabled = false;
+            this.YMax.Location = new System.Drawing.Point(452, 723);
+            this.YMax.Name = "YMax";
+            this.YMax.Size = new System.Drawing.Size(45, 30);
+            this.YMax.TabIndex = 13;
+            // 
+            // Semicolon2
+            // 
+            this.Semicolon2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Semicolon2.AutoSize = true;
+            this.Semicolon2.Location = new System.Drawing.Point(436, 726);
+            this.Semicolon2.Name = "Semicolon2";
+            this.Semicolon2.Size = new System.Drawing.Size(20, 22);
+            this.Semicolon2.TabIndex = 12;
+            this.Semicolon2.Text = ";";
+            // 
+            // YMin
+            // 
+            this.YMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.YMin.Enabled = false;
+            this.YMin.Location = new System.Drawing.Point(391, 723);
+            this.YMin.Name = "YMin";
+            this.YMin.Size = new System.Drawing.Size(45, 30);
+            this.YMin.TabIndex = 11;
+            // 
+            // AxisYLbl
+            // 
+            this.AxisYLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.AxisYLbl.AutoSize = true;
+            this.AxisYLbl.Location = new System.Drawing.Point(294, 726);
+            this.AxisYLbl.Name = "AxisYLbl";
+            this.AxisYLbl.Size = new System.Drawing.Size(100, 22);
+            this.AxisYLbl.TabIndex = 10;
+            this.AxisYLbl.Text = "Axis Y: [";
+            // 
+            // AutoY
+            // 
+            this.AutoY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.AutoY.AutoSize = true;
+            this.AutoY.Checked = true;
+            this.AutoY.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AutoY.Location = new System.Drawing.Point(518, 725);
+            this.AutoY.Name = "AutoY";
+            this.AutoY.Size = new System.Drawing.Size(69, 26);
+            this.AutoY.TabIndex = 15;
+            this.AutoY.Text = "Auto";
+            this.AutoY.UseVisualStyleBackColor = true;
+            this.AutoY.CheckedChanged += new System.EventHandler(this.AutoY_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1017, 717);
+            this.ClientSize = new System.Drawing.Size(1017, 756);
+            this.Controls.Add(this.AutoY);
+            this.Controls.Add(this.RightBracket2);
+            this.Controls.Add(this.YMax);
+            this.Controls.Add(this.Semicolon2);
+            this.Controls.Add(this.YMin);
+            this.Controls.Add(this.AxisYLbl);
+            this.Controls.Add(this.RightBracket1);
+            this.Controls.Add(this.XMax);
+            this.Controls.Add(this.Semicolon1);
+            this.Controls.Add(this.XMin);
+            this.Controls.Add(this.AxisXLbl);
             this.Controls.Add(this.BuildBtn);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.YEqLbl);
@@ -153,6 +285,17 @@
         private System.Windows.Forms.Button BuildBtn;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.Label AxisXLbl;
+        private System.Windows.Forms.TextBox XMin;
+        private System.Windows.Forms.Label Semicolon1;
+        private System.Windows.Forms.TextBox XMax;
+        private System.Windows.Forms.Label RightBracket1;
+        private System.Windows.Forms.Label RightBracket2;
+        private System.Windows.Forms.TextBox YMax;
+        private System.Windows.Forms.Label Semicolon2;
+        private System.Windows.Forms.TextBox YMin;
+        private System.Windows.Forms.Label AxisYLbl;
+        private System.Windows.Forms.CheckBox AutoY;
     }
 }
 
--- GraphMaker/Form1.cs
+++ GraphMaker/Form1.cs
@@ -7,20 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Threading;
 
 namespace GraphMaker
 {
     public partial class Form1 : Form
     {
-        string Mul { get; } = "*";//константа строки умножения
-        string Plus { get; } = "+";//константа строки плюса
-        string Minus { get; } = "-";//константа строки минуса
-        string Div { get; } = "/";//константа строки деления
+        const string Mul = "*";//константа строки умножения
+        const string Plus = "+";//константа строки плюса
+        const string Minus = "-";//константа строки минуса
+        const string Div = "/";//константа строки деления
 
-        char mul { get; } = '*';//константа символа умножения
-        char plus { get; } = '+';//константа символа плюса
-        char minus { get; } = '-';//константа символа минуса
-        char div { get; } = '/';//константа символа деления
+        const char mul = '*';//константа символа умножения
+        const char plus = '+';//константа символа плюса
+        const char minus = '-';//константа символа минуса
+        const char div = '/';//константа символа деления
+
+        readonly string[][] ranks = new[] { new[] { Plus, Minus }, new[] { Mul, Div, "Mod" }, new[] { "^", "yroot" } };//создание рангов операций
+
+        double[] range = {-10.0, -9.9, -9.8, -9.7, -9.6, -9.5, -9.4, -9.3, -9.2, -9.1,
+                           -9.0, -8.9, -8.8, -8.7, -8.6, -8.5, -8.4, -8.3, -8.2, -8.1,
+                           -8.0, -7.9, -7.8, -7.7, -7.6, -7.5, -7.4, -7.3, -7.2, -7.1,
+                           -7.0, -6.9, -6.8, -6.7, -6.6, -6.5, -6.4, -6.3, -6.2, -6.1,
+                           -6.0, -5.9, -5.8, -5.7, -5.6, -5.5, -5.4, -5.3, -5.2, -5.1,
+                           -5.0, -4.9, -4.8, -4.7, -4.6, -4.5, -4.4, -4.3, -4.2, -4.1,
+                           -4.0, -3.9, -3.8, -3.7, -3.6, -3.5, -3.4, -3.3, -3.2, -3.1,
+                           -3.0, -2.9, -2.8, -2.7, -2.6, -2.5, -2.4, -2.3, -2.2, -2.1,
+                           -2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1,
+                           -1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1,
+                            0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+                            1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9,
+                            2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9,
+                            3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9,
+                            4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9,
+                            5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9,
+                            6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9,
+                            7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9,
+                            8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9,
+                            9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10.0 };
 
         public Form1()
         {
@@ -29,37 +54,79 @@ namespace GraphMaker
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            GraphicChart.ChartAreas[0].CursorX.IsUserEnabled = true;
+            GraphicChart.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            GraphicChart.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            GraphicChart.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
 
+            GraphicChart.ChartAreas[0].CursorY.IsUserEnabled = true;
+            GraphicChart.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            GraphicChart.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            GraphicChart.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
         }
 
         private void BuildBtn_Click(object sender, EventArgs e)
         {
-            GraphicChart.Series[0].Points.Clear();
-            GraphicChart.ChartAreas[0].AxisX.Minimum = -10;
-            GraphicChart.ChartAreas[0].AxisX.Maximum = 10;
-            GraphicChart.ChartAreas[0].AxisX.MajorGrid.Interval = 1;
-            for (double i = -10; i <= 10; i += 0.1)
+            GraphicChart.Series.Clear();
+            GraphicChart.Series.Add(new Series { ChartType = SeriesChartType.Spline, Name = textBox1.Text, Color = Color.Blue });
+            GraphicChart.ChartAreas[0].AxisX.Minimum = Convert.ToDouble(XMin.Text);
+            GraphicChart.ChartAreas[0].AxisX.Maximum = Convert.ToDouble(XMax.Text);
+            if (!AutoY.Checked)
             {
-                string txt = textBox1.Text.Replace("x", i.ToString());
-                double y = Solution(txt);
-                if (!double.IsNaN(y) && !double.IsInfinity(y))
-                    GraphicChart.Series[0].Points.AddXY(i, y);
+                GraphicChart.ChartAreas[0].AxisY.Minimum = Convert.ToDouble(YMin.Text);
+                GraphicChart.ChartAreas[0].AxisY.Maximum = Convert.ToDouble(YMax.Text);
             }
+            double step = (Math.Abs(Convert.ToDouble(XMax.Text)) + Math.Abs(Convert.ToDouble(XMin.Text))) / 200.0;
+            GraphicChart.ChartAreas[0].AxisX.MajorGrid.Interval = step * 10;
+            if (step != 0.1)
+                for (double i = Convert.ToDouble(XMin.Text); i <= Convert.ToDouble(XMax.Text); i += step)
+                {
+                    string txt = textBox1.Text.Replace("x", i.ToString());
+                    double y = Solution(txt);
+                    if (double.IsNaN(y) || double.IsInfinity(y))
+                    {
+                        GraphicChart.Series.Add(new Series
+                        {
+                            ChartType = SeriesChartType.Spline,
+                            IsVisibleInLegend = false,
+                            Color = Color.Blue
+                        });
+                        continue;
+                    }
+                    GraphicChart.Series[GraphicChart.Series.Count - 1].Points.AddXY(i, y);
+                }
+            else
+                foreach (double i in range)
+                {
+                    string txt = textBox1.Text.Replace("x", i.ToString());
+                    double y = Solution(txt);
+                    if (double.IsNaN(y) || double.IsInfinity(y))
+                    {
+                        GraphicChart.Series.Add(new Series
+                        {
+                            ChartType = SeriesChartType.Spline,
+                            IsVisibleInLegend = false,
+                            Color = Color.Blue
+                        });
+                        continue;
+                    }
+                    GraphicChart.Series[GraphicChart.Series.Count - 1].Points.AddXY(i, y);
+                }
         }
 
         private double Solution(string text)
         {
             double Result = 0;
-            text = string.Join(" ", text.Split(), 0, new string[] { Plus, Minus, Mul, Div, "Mod", "^", "yroot" }
-                                                                  .Any(x => x == text.Split().Last()) ? text.Split().Length - 1 : text.Split().Length)
+            text = string.Join(" ", text.Split(), 0, new[] { Plus, Minus, Mul, Div, "Mod", "^", "yroot" }
+                                                           .Any(x => x == text.Split().Last()) ? text.Split().Length - 1 : text.Split().Length)
                               .Remove(0, text.Length > 0 && text[0] == ' ' ? 1 : 0);//форматирование/подготовка строки для решения
-            try //если решать ничего не нужно
+            try//если решать ничего не нужно
             {
                 Result = Convert.ToDouble(text);
             }
             catch//решаем...
             {
-                if (text.Contains("(")) //handler brackets
+                if (text.Contains("("))//handler brackets
                 {
                     int ibegin = -1, iend = -1;//временные данные
                     text += new string(')', text.Count(x => x == '(') - text.Count(x => x == ')'));//выравнивание скобок
@@ -76,7 +143,7 @@ namespace GraphMaker
                             }
                         if (ibegin > 0 && text[ibegin - 1] != ' ')//получение функции
                         {
-                            for (int i = ibegin - 1; i >= 0 && text[i] != ' '; i--)
+                            for (int i = ibegin - 1; i >= 0 && text[i] != ' ' && text[i] != '('; i--)
                                 ibegin--;
                             Result = SolutionFunc(text.Remove(iend).Remove(0, ibegin) + ")");//и отправление ее на решение
                         }
@@ -86,20 +153,16 @@ namespace GraphMaker
                     }
                 }
                 string[] txt = text.Split();//разделение строки на подстроки типа: число, операция, число, ...
-                if (txt.Length < 3)
+                if (txt.Length < 3)//because the method is recursive can come number
                 {
                     Result = Convert.ToDouble(text);
                     return Result;
                 }
-                string[][] ranks = new string[][] {
-                        new string[] { Plus, Minus },
-                        new string[] { Mul, Div, "Mod" },
-                        new string[] { "^", "yroot" } };//создание рангов операций
-                for (int i = txt.Length - 1, rank = 0; i > 0; i--)
+                int rank = 0;
+                for (int i = txt.Length - 1; i > 0; i--)//check from the end
                 {
                     if (ranks[rank].Any(x => x == txt[i]))//если на актуальном ранге есть совпадение операций
                     {
-
                         double num1 = Solution(string.Join(" ", txt, 0, i));//рекурсия для выявления первого операнда
                         double num2 = Solution(string.Join(" ", txt, i + 1, txt.Length - i - 1));//рекурсия для выявления второго операнда
                         if (rank == 0)//разделение вычислений на ранги
@@ -110,10 +173,10 @@ namespace GraphMaker
                             Result = ranks[rank][0] == txt[i] ? Math.Pow(num1, num2) : Math.Pow(num1, 1.0 / num2);//^ or yroot
                         else//overflow of rank
                         {
-                            MessageBox.Show("Code #2:\n" + $@"""{text}""" + "\n...\nUnknown error\nFor continued press OK.", "Error");
+                            MessageBox.Show("Code #2:\n" + $@"""{text}""" + "\n...\nOverflow Of Rank Error\nFor continued press OK.", "Error");
                             //buttonCancel.PerformClick();
                         }
-                        break;//если нашли хоть одно совпадение оперций - это конечная, дальше дело за рекурсиями
+                        break;//если нашли хоть одно совпадение оперций - это конечная, дальше дело за рекурсией
                     }
                     if (i == 1)//повышение ранга
                     {
@@ -125,8 +188,8 @@ namespace GraphMaker
             return Result;
         }
 
-        private double SolutionFunc(string text) //text is briefly func. Example: "fact(5)" will "120"
-        {//решение функций(хорошо что я решил это делать в отдельном методе, а то в Solution'е итак много всякого) 
+        private double SolutionFunc(string text)
+        {//решение функций(а то в Solution'е итак много всякого) 
             if (text[0] == ' ')
                 text = text.Remove(0, 1);
             int ibegin = 0, iend = 0;
@@ -178,6 +241,12 @@ namespace GraphMaker
                 case "e^"://возведение в экспаненту
                     Result = Math.Exp(Result);
                     break;
+                case "-":
+                    Result = -Result;
+                    break;
+                case "abc":
+                    Result = Math.Abs(Result);
+                    break;
                 //trigonometry func
                 case "sin":
                     Result = Math.Sin(Result);
@@ -195,7 +264,7 @@ namespace GraphMaker
                     Result = 1 / Math.Tan(Result);
                     break;
                 case "ctg":
-                    Result = 1 / Math.Tan(Result);
+                    Result = Math.Cos(Result) / Math.Sin(Result);
                     break;
                 //arc trigonometry func
                 case "asin":
@@ -262,6 +331,20 @@ namespace GraphMaker
                     break;
             }
             return Result;
+        }
+
+        private void AutoY_CheckedChanged(object sender, EventArgs e)
+        {
+            if (AutoY.Checked)
+            {
+                YMin.Enabled = false;
+                YMax.Enabled = false;
+            }
+            else
+            {
+                YMin.Enabled = true;
+                YMax.Enabled = true;
+            }
         }
     }
 }
